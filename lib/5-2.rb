933b9b25361986ad9a8c5eed441f471f40ac7d0f
Charles Greene <charles.greene@redspace.com>
2022-12-05 08:51:44 -0400

    Add Day 5

---
+++ lib/5-2.rb
@@ -0,0 +1,33 @@
+module D5P2
+  class D5P2
+    NO_STRIP = true
+    def run(input)
+      stacks = Array.new(input.first.size/4){Array.new()}
+      mode = :stacks
+      input.each do |line|
+        case mode
+        when :stacks
+          if line[1].to_i != 0
+            mode = :orders
+            next
+          end
+          for i in 0..(line.size/4-1)
+            char = line[(i*4)+1]
+            stacks[i].push(char) unless char.strip.empty?
+          end
+        when :orders
+          next if line.strip.empty?
+          line = line.split(" ")
+          target = line[5].to_i-1
+          moved = stacks[line[3].to_i-1].shift(line[1].to_i)
+          stacks[target] = moved + stacks[target]
+        end
+      end
+      code = ""
+      stacks.each do |stack|
+        code += stack.shift
+      end
+      puts "Code: #{code}"
+    end
+  end
+end
